6bf91936c2d98baa8d2c34b36dcb20e2cf016be3
KevinMonachan <kev061201@gmail.com>
2025-03-08 16:49:06 +0530

    week2

--- CeaserCipher.py
+++ CeaserCipher.py
@@ -1,6 +1,3 @@
-import art
-print(art.logo)
-
 alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 def ceaser(text,shift_amount,cipher_direction):
     output_text=""
