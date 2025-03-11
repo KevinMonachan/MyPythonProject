3d5af430dfa4e09d47e203821de8609c8a393328
KevinMonachan <kev061201@gmail.com>
2025-03-12 00:39:37 +0530

    day2

--- TheSecretAuction.py
+++ TheSecretAuction.py
@@ -1,6 +1,27 @@
-name=input("What is your name? ")
-print(f"Welcome to the secret auction program {name}")
-price=int(input("What is your bid? $"))
-bids={
+def find_highest_bid(bidding_dictionary):
+    highest_bid=0
+    highest_bidder=""
 
-}
+    for bidder in bidding_dictionary:
+       if bidding_dictionary[bidder] > highest_bid:
+          highest_bidder=bidder  
+          highest_bid=bidding_dictionary[bidder]
+
+    print(f"The winner is {bidder} with a bid of {highest_bid}")
+
+bids={}
+continue_bidding=True
+while continue_bidding:
+    name=input("Enter Your name: ")
+    print(f"Welcome to the secret auction program {name}")
+    price=int(input("Enter your bid: "))
+    bids[name]=price
+    should_continue=input("Are there any other bidders? Type 'yes'or'no': ").lower()
+    if should_continue=="no":
+       continue_bidding=False
+       find_highest_bid(bids)
+    else:
+        continue_bidding=True
+        print("\n"*20)
+
+  
